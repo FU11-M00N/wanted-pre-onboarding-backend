349247007f3bea987114ee4a87d5147461b2293e
FU11-M00N <mmmhobin3@gmail.com>
2023-08-04 00:04:33 +0900

    feat: 프로젝트 초기 설정

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+app.set('port', process.env.PORT || 3001);
+
+app.get('/', (req, res) => {
+   res.send('test');
+});
+
+app.listen(app.get('port'), () => {
+   console.log(app.get('port'));
+});
