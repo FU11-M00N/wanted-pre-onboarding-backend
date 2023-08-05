5ce270e410daae9f5ab3fb09edd278443887882e
FU11-M00N <mmmhobin3@gmail.com>
2023-08-05 16:48:42 +0900

    feat: connect db

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const express = require('express');
 
+const morgan = require('morgan');
 const app = express();
+
+const { sequelize } = require('./models/index');
+
+sequelize
+   .sync({ force: false })
+   .then(() => {
+      console.log('DB 연결');
+   })
+   .catch(err => {
+      console.error(err);
+   });
+
+app.use(morgan('dev'));
+
+require('dotenv').config();
+
 app.set('port', process.env.PORT || 3001);
 
 app.get('/', (req, res) => {
---
+++ models/index.js
@@ -0,0 +1,32 @@
+const Sequelize = require('sequelize');
+const fs = require('fs');
+const path = require('path');
+const env = process.env.NODE_ENV || 'development';
+const config = require('../config/config')[env];
+
+const db = {};
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
+
+db.sequelize = sequelize;
+
+const basename = path.basename(__filename);
+fs.readdirSync(__dirname) // 현재 폴더의 모든 파일을 조회
+   .filter(file => {
+      return file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js';
+   })
+   .forEach(file => {
+      // init
+      const model = require(path.join(__dirname, file));
+      console.log(file, model.name);
+      db[model.name] = model;
+      model.initiate(sequelize);
+   });
+
+Object.keys(db).forEach(modelName => {
+   // associate 호출
+   if (db[modelName].associate) {
+      db[modelName].associate(db);
+   }
+});
+
+module.exports = db;
